1d6a8393895a39d5c086819181ef93622a225a36
antoinethebuilder <64340767+antoinethebuilder@users.noreply.github.com>
2020-07-06 11:14:59 -0400

    add ilm_fortigate policy, basic queries

--- setup/gen-password.sh
+++ setup/gen-password.sh
@@ -19,20 +19,26 @@ echo "Creating 'logstash_writer' role..."
 curl -X POST --cacert ${CACERT} \
 	-u "elastic:${ELASTIC_PASSWORD}" -s -o /dev/null \
 	-H "Content-Type: application/json" \
-	-d @setup/roles/role-logstash_writer.json "${es_url}/_xpack/security/role/logstash_writer"
+	-d @setup/roles/role-logstash_writer.json "${es_url}/_security/role/logstash_writer"
 
 echo "Creating 'logstash_internal' user..."
 curl --cacert ${CACERT} -u \
 	"elastic:${ELASTIC_PASSWORD}" -s -o /dev/null \
-	-X POST $es_url/_xpack/security/user/logstash_internal \
+	-X POST $es_url/_security/user/logstash_internal \
 	-H "Content-Type: application/json" \
 	--data-binary '{"password":"'"$LOGSTASH_WRITER_PASSWORD"'","roles":["logstash_writer"],"full_name":"Internal Logstash User"}'
 
-echo "Installing fortigate index template..."
+echo "Creating the Index Lifecycle policy for Fortigate..."
 curl -X PUT --cacert ${CACERT} \
         -u "elastic:${ELASTIC_PASSWORD}" -s -o /dev/null \
         -H "Content-Type: application/json" \
-        -d @setup/templates/fortigate-6.2.2.json "${es_url}/_template/fortigate?pretty"
+        -d @setup/templates/policies/ilm-fortigate.json "${es_url}/_ilm/policy/ilm_fortigate
+
+echo "Installing Fortigate index template..."
+curl -X PUT --cacert ${CACERT} \
+        -u "elastic:${ELASTIC_PASSWORD}" -s -o /dev/null \
+        -H "Content-Type: application/json" \
+        -d @setup/templates/index/fortigate-6.2.2.json "${es_url}/_template/fortigate?pretty"
 
 echo "Generating Elasticsearch Stack passwords..."
 docker exec -it $(docker container ls -qf "name=elastic_elasticsearch") \
